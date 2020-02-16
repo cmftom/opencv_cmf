d705ed4ba4188a115e2e19ff46d31a88892356b8
cmftom <38613993+cmftom@users.noreply.github.com>
2020-02-16 17:38:48 +0800

    Update and rename opencv.sh to install_CV.sh

---
+++ install_CV.sh
@@ -0,0 +1,43 @@
+sudo apt-get -y purge wolfram-engine
+sudo apt-get -y purge libreoffice*
+sudo apt-get -y clean
+sudo apt-get -y autoremove
+
+# Update the pi
+sudo apt -y update
+
+# Install the pakeage
+sudo apt -y install build-essential cmake git pkg-config libgtk-3-dev "libcanberra-gtk*"
+sudo apt -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
+sudo apt -y install libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev opencl-headers
+sudo apt -y install python3-dev python3-numpy libtbb2 libtbb-dev libdc1394-22-dev
+
+# Create build directory
+mkdir ~/opencv_build && cd ~/opencv_build
+
+# Download the opencv
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+
+# Create temp build
+mkdir -p ~/opencv_build/opencv/build && cd ~/opencv_build/opencv/build
+
+# CMAKE
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_C_EXAMPLES=OFF \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
+    -D OPENCV_GENERATE_PKGCONFIG=ON \
+    -D ENABLE_NEON=ON \
+    -D OPENCV_EXTRA_EXE_LINKER_FLAGS=-latomic \
+    -D ENABLE_VFPV3=ON \
+    -D BUILD_TESTS=OFF \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_build/opencv_contrib/modules \
+    -D BUILD_EXAMPLES=OFF ..
+
+# Make
+make -j4
+
+# Install
+sudo make install
